a769510a3a3aa0b22fe1fca9160f87523eba5334
Biplo12 <robertbiplosek@gmail.com>
2022-05-14 15:16:37 +0200

    Change from styled-components to sass | responsive navbar

--- Client/src/Components/Navbar.jsx
+++ Client/src/Components/Navbar.jsx
@@ -1,149 +1,65 @@
-import React from "react";
+import react, { useState } from "react";
 import { Link } from "react-router-dom";
-import styled from "styled-components";
 import cartIcon from "../Assets/cart.svg";
 import searchIcon from "../Assets/search.svg";
 import menuIcon from "../Assets/menu.svg";
-import { mobile } from "../responsive";
-const NavbarContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 95%;
-  margin: 0 auto;
-  height: 70px;
-  ${mobile({ height: "50px" })}
-`;
-
-const NavbarLeft = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 30%;
-  @media screen and (max-width: 1240px) {
-    width: 90%;
-  }
-`;
-
-const NavbarLogo = styled.div``;
-
-const StyledLink = styled(Link)`
-  ${mobile({ fontSize: "12px" })}
-  text-decoration: none;
-  text-align: center;
-  &:focus,
-  &:hover,
-  &:visited,
-  &:link,
-  &:active {
-    text-decoration: none;
-  }
-`;
-
-const NavbarSearchContainer = styled.div`
-  padding: 5px 10px;
-  border: 1px solid white;
-  border-radius: 50px;
-  outline: none;
-  display: flex;
-`;
-
-const NavbarSearch = styled.input`
-  border: none;
-  outline: none;
-`;
-
-const NavbarRight = styled.div`
-  display: flex;
-  justify-content: end;
-  align-items: center;
-  width: 70%;
-  @media screen and (max-width: 1240px) {
-    width: 10%;
-  }
-`;
-
-const NavbarList = styled.ul`
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  @media screen and (max-width: 1240px) {
-    display: none;
-  }
-`;
-
-const NavbarMenuItem = styled.li`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 25px;
-`;
-
-<style>menuIconHamburger{{ display: "none" }}</style>;
+import navbarStyles from "./Styles/navbar.scss";
 
 const Navbar = () => {
+  // const [mobileMenu, setMobileMenu] = useState(false);
+
   return (
-    <NavbarContainer>
-      <NavbarLeft>
-        <NavbarLogo>
-          <h3>e-commerce</h3>
-        </NavbarLogo>
-        <NavbarSearchContainer>
-          <NavbarSearch type="text" placeholder="Search" />
-          <img src={searchIcon} alt="search" width={"15px"} />
-        </NavbarSearchContainer>
-      </NavbarLeft>
-      <NavbarRight>
-        <NavbarList>
-          <StyledLink to="/Home">
-            <NavbarMenuItem>
-              <p>Home</p>
-            </NavbarMenuItem>
-          </StyledLink>
-          <StyledLink to="/new-collecion">
-            <NavbarMenuItem>
-              <p>New collection</p>
-            </NavbarMenuItem>
-          </StyledLink>
-          <StyledLink to="/men">
-            <NavbarMenuItem>
-              <p>For men</p>
-            </NavbarMenuItem>
-          </StyledLink>
-          <StyledLink to="/woman">
-            <NavbarMenuItem>
-              <p>For woman</p>
-            </NavbarMenuItem>
-          </StyledLink>
-          <StyledLink to="/accessories">
-            <NavbarMenuItem>
-              <p>Accessories</p>
-            </NavbarMenuItem>
-          </StyledLink>
-          <StyledLink to="/sign-up">
-            <NavbarMenuItem>
-              <p>SIGN UP</p>
-            </NavbarMenuItem>
-          </StyledLink>
-          <StyledLink to="/sign-in">
-            <NavbarMenuItem>
-              <p>SIGN IN</p>
-            </NavbarMenuItem>
-          </StyledLink>
-          <StyledLink to="/cart">
-            <NavbarMenuItem>
-              <img src={cartIcon} alt="" width={"20px"} />
-            </NavbarMenuItem>
-          </StyledLink>
-          <img
-            src={menuIcon}
-            alt="hamburgerMenu"
-            width={"20px"}
-            className="menuIconHamburger"
-          />
-        </NavbarList>
-      </NavbarRight>
-    </NavbarContainer>
+    <div className="navbar-container">
+      <div className="navbar-left">
+        <div className="navbar-logo-search-wrapper">
+          <div className="navbar-logo">
+            <h3>e-commerce</h3>
+          </div>
+          <div className="navbar-search-container">
+            <input type="text" placeholder="Search" />
+            <img src={searchIcon} alt="Search icon" width="20px" />
+          </div>
+        </div>
+      </div>
+      <div className="navbar-right">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/new-collection">New Collection</Link>
+          </li>
+          <li>
+            <Link to="/men">For men</Link>
+          </li>
+          <li>
+            <Link to="/woman">For woman</Link>
+          </li>
+          <li>
+            <Link to="/register">SIGN UP</Link>
+          </li>
+          <li>
+            <Link to="/login">SIGN IN</Link>
+          </li>
+          <li>
+            <img
+              src={cartIcon}
+              alt="Cart icon"
+              className="cartIcon"
+              width="20px"
+            />
+          </li>
+          <li>
+            <img
+              src={menuIcon}
+              alt="Menu icon"
+              className="menuIcon"
+              width="20px"
+            />
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
--- Client/src/Components/Slider.jsx
+++ Client/src/Components/Slider.jsx
@@ -1,47 +1,7 @@
 import React from "react";
-import styled from "styled-components";
-
-const SliderContainer = styled.div`
-  width: 100%;
-  min-height: calc(100vh - 70px);
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const SliderItemsContainer = styled.div``;
-const SliderNavNumber = styled.div``;
-const SliderTitle = styled.div``;
 
 const Slider = () => {
-  return (
-    <SliderContainer>
-      <SliderItemsContainer>
-        <SliderNavNumber>
-          <p>01</p>
-        </SliderNavNumber>
-        <SliderTitle>Beige</SliderTitle>
-      </SliderItemsContainer>
-      <SliderItemsContainer>
-        <SliderNavNumber>
-          <p>02</p>
-        </SliderNavNumber>
-        <SliderTitle>Beige</SliderTitle>
-      </SliderItemsContainer>
-      <SliderItemsContainer>
-        <SliderNavNumber>
-          <p>03</p>
-        </SliderNavNumber>
-        <SliderTitle>Beige</SliderTitle>
-      </SliderItemsContainer>
-      <SliderItemsContainer>
-        <SliderNavNumber>
-          <p>04</p>
-        </SliderNavNumber>
-        <SliderTitle>Beige</SliderTitle>
-      </SliderItemsContainer>
-    </SliderContainer>
-  );
+  return <></>;
 };
 
 export default Slider;
