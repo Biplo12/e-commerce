fdee07ed1b702048f5a183b0c647a2379d2f43e2
Biplo12 <robertbiplosek@gmail.com>
2022-05-23 22:46:49 +0200

    Cart page PC design finished

--- Client/src/Components/CartContent.jsx
+++ Client/src/Components/CartContent.jsx
@@ -3,46 +3,97 @@ import React from "react";
 const CartContent = () => {
   return (
     <div className="cart-container">
-      <div className="cart-left">
-        <h5>Your bag</h5>
+      <div className="cart-link-back">
+        {" "}
         <a href="/products">
           <span className="un">Keep shopping</span>
         </a>
       </div>
-      <div className="cart-right">
-        <table>
-          <thead>
-            <tr>
-              <th>Product</th>
-              <th>Color</th>
-              <th>Size</th>
-              <th>QTY</th>
-              <th>Total price</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                <a href="">
-                  <img
-                    src="./Assets/Product/example-1.png"
-                    alt="product in cart"
-                    width="50px"
-                  />
-                  <span className="un"> BOWLING SHIRT BLACK</span>
-                </a>
-              </td>
-              <td>Black</td>
-              <td>L</td>
-              <td className="cart-product-qty">
-                <span>+</span>
-                <h5>1</h5>
-                <span>-</span>
-              </td>
-              <td>$355</td>
-            </tr>
-          </tbody>
-        </table>
+      <div className="cart-title">
+        <h5>Your bag</h5>
+      </div>
+      <div className="cart-table">
+        <div className="cart-table-headlines">
+          <div>
+            <h3>Description</h3>
+          </div>
+          <div>
+            <h3>Size</h3>
+          </div>
+          <div>
+            <h3>Color</h3>
+          </div>
+          <div>
+            <h3>Quantity</h3>
+          </div>
+          <div>
+            <h3>Remove</h3>
+          </div>
+          <div>
+            <h3>Price</h3>
+          </div>
+        </div>
+        <div className="cart-table-item">
+          <div className="product-description">
+            <img
+              src="./Assets/Product/example-1.png"
+              alt="product cart"
+              width="100px"
+            />
+            <h2>Bowling T-shirt adsd</h2>
+          </div>
+          <div className="cart-small-box">
+            <h2>L</h2>
+          </div>
+          <h2>Black</h2>
+          <div className="product-quantity">
+            <div className="cart-small-box">
+              <span>+</span>
+            </div>
+            <div className="cart-small-box">
+              <h2>1</h2>
+            </div>
+            <div className="cart-small-box">
+              <span>-</span>
+            </div>
+          </div>
+          <div className="cart-small-box">
+            <h2>x</h2>
+          </div>
+          <h2>$355</h2>
+        </div>
+      </div>
+      <div className="cart-bottom-boxes">
+        <div className="cart-boxes-wrapper">
+          <div className="cart-discount big-box">
+            <h4>Discount: </h4>
+            <h4>$0.00 </h4>
+          </div>
+          <div className="cart-delivery big-box">
+            <h4>Delivery: </h4>
+            <h4>$0.00 </h4>
+          </div>
+          <div className="cart-subtotal big-box">
+            <h4>Subtotal: </h4>
+            <h4>$355.00 </h4>
+          </div>
+          <div className="cart-total big-box">
+            <h4>Total: </h4>
+            <h4>$355.00 </h4>
+          </div>
+        </div>
+      </div>
+      <div className="cart-bottom-buttons">
+        <div className="cart-bottom-input">
+          <p>If you have a promotion code, please enter it below:</p>
+          <input type="text" placeholder="Enter your code here" />
+          <button type="submit">
+            <span className="un"> Apply Discount</span>
+          </button>
+        </div>
+        <div className="cart-bottom-checkout">
+          <button type="submit">Checkout</button>
+        </div>
       </div>
     </div>
   );
