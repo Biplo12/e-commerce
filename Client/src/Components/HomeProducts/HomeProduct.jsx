1f1893bfffaa2df987b91c38b1f684db9d850786
Biplo12 <75808735+Biplo12@users.noreply.github.com>
2022-06-10 23:30:39 +0200

    Hero Products responsive

--- Client/src/Components/HomeProducts/HomeProduct.jsx
+++ Client/src/Components/HomeProducts/HomeProduct.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 const HomeProduct = ({ image, productId }) => {
   return (
-    <Link to={`/product/${productId}`}>
-      <div className="home-product">
+    <div className="home-product">
+      <Link to={`/product/${productId}`}>
         <div className="home-product-image">
           <img src={image} alt="product home" />
         </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 };
 
